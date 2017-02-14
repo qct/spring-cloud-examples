6891649f782a67505c7a95655313c51c1b309be7
alex <tango.qct@gmail.com>
2017-02-14 09:24:28 +0800

    eureka and config

---
+++ config-server/src/main/java/alex/Application.java
@@ -0,0 +1,17 @@
+package alex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * <p>Created by Damon.Q on 2017/2/13.
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ eureka-service/src/main/java/alex/EurekaServiceApplication.java
@@ -0,0 +1,17 @@
+package alex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * <p>Created by Damon.Q on 2017/2/13.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServiceApplication.class, args);
+    }
+}
